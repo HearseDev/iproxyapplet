184c2ad36960f4f8db28fb2988e09fd39e23ffe7
HearseDev <hearsd@gmail.com>
2022-02-21 14:14:30 -0800

    Add icon. Enable ssh by default. Improve iproxy install check. Terminate processes on exit if alive. etc.

--- systray.py
+++ systray.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
 from pystray import Icon as icon, Menu as menu, MenuItem as item
-from PIL import Image, ImageDraw
+from PIL import Image
 import subprocess
-width=50
-height=50
-color1='white'
-color2='black'
-def create_image():
-    # Generate an image and draw a pattern
-    image = Image.new('RGB', (width, height), color1)
-    dc = ImageDraw.Draw(image)
-    dc.rectangle(
-        (width // 2, 0, width, height // 2),
-        fill=color2)
-    dc.rectangle(
-        (0, height // 2, width // 2, height),
-        fill=color2)
+from sys import platform
+from shutil import which
 
-    return image
 
-installed = True if subprocess.run(["which","iproxy"]).returncode == 0 else False
+#disable python launcher icon on macos
+if platform == "darwin":
+    print("darwin detected, enabling LSBackgroundOnly")
+    import AppKit
+    info = AppKit.NSBundle.mainBundle().infoDictionary()
+    info["LSBackgroundOnly"] = "1"
+
+#globals
+installed = which("iproxy") is not None
 running_state = False
-ssh = False
+ssh = True
 debug = False
-ssh_process : subprocess.Popen
-debug_process : subprocess.Popen
+ssh_process = None
+debug_process = None
 
+def create_image():
+    image = Image.open('icon.png')
+    return image
 
 def ssh_clicked(icon, item):
     global ssh
@@ -42,8 +40,10 @@ def start_clicked(icon, item):
     running_state = True
     if ssh:
         ssh_process = subprocess.Popen("iproxy 2222 22", shell=True)
+        print("Started ssh_process with pid", ssh_process.pid)
     if debug:
         debug_process = subprocess.Popen("iproxy 1234 1234", shell=True)
+        print("Started debug_process with pid", debug_process.pid)
 
 def stop_clicked(icon, item):
     global running_state
@@ -51,19 +51,20 @@ def stop_clicked(icon, item):
     global debug_thread
     if running_state:
         if ssh:
+            print("Terminating ssh_process with pid", ssh_process.pid)
             ssh_process.terminate()
         if debug:
+            print("Terminating debug_process with pid", debug_process.pid)
             debug_process.terminate()
     running_state = False
 
 def get_radio_state(v):
     def inner(item):
-        if running_state:
+        if running_state and installed:
             return False
-        elif not running_state:
+        elif not running_state and installed:
             return True
     return inner
-
 
 def get_start_state(v):
     def inner(item):
@@ -84,9 +85,19 @@ def get_stop_state(v):
         else:
             return True
     return inner
-def running_status(v):
+def running_status(_):
     return 'Running' if running_state else 'Not Running'
-
+def exit_clicked(icon):
+    if ssh_process is not None:
+        if ssh_process.poll() is None:
+            print("Terminating ssh_process with pid", ssh_process.pid)
+            ssh_process.terminate()
+    if debug_process is not None:
+        if debug_process.poll() is None:
+            print("Terminating debug_process with pid", debug_process.pid)
+            debug_process.terminate()
+    icon.stop()
+    
 
 # Update the state in `on_clicked` and return the new state in
 # a `checked` callable
@@ -97,4 +108,4 @@ icon('iProxy', create_image(), menu=menu(
     item('debug -> 1234',debug_clicked,checked=lambda item: debug, enabled=get_radio_state(item.enabled)),
     item('Start',start_clicked,enabled=get_start_state(item.enabled)),
     item('Stop',stop_clicked,enabled=get_stop_state(item.enabled)),
-    item('Exit',action=lambda icon:icon.stop()))).run()
+    item('Exit',exit_clicked))).run()
